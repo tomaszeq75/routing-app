6de1246e3be29c635f0d7c01e2f4f4239b5758c6
Tomek <t.wolyniec@tlen.pl>
2021-06-06 15:17:32 +0200

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {HomeComponent} from "./component/home/home.component";
+import {AboutComponent} from "./component/about/about.component";
+import {ContactComponent} from "./component/contact/contact.component";
+import {PageNotFoundComponent} from "./component/page-not-found/page-not-found.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
